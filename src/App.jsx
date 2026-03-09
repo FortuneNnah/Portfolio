6ae94c75fd5a20e5c8c6b40f38cec4b65b29a112
Fortune Nnah <Fortunennah747@gmail.com>
2026-03-09 22:18:05 +0100

    started about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
-import Main from './Components/Main'
+import "./App.css";
+import Main from "./Components/Main";
+import About from "./Components/About";
 
-function App(){
-  return(
-    <Main />
-  )
+function App() {
+  return (
+    <>
+      <Main />
+      <About />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-content">
+        <div className="text">
+          <h1>About Me</h1>
+          <p>
+            I’m a frontend developer passionate about building modern,
+            responsive, and user-focused web experiences. I enjoy transforming
+            ideas and designs into functional, accessible interfaces using
+            technologies like HTML, CSS, JavaScript, and React. I focus on
+            writing clean, maintainable code and creating websites that are not
+            only visually appealing but also fast and performant. I'm constantly
+            learning and improving my skills while exploring new tools and
+            technologies in the frontend ecosystem.
+
+            I’m constantly learning, experimenting, and improving my React skills to build better digital experiences.
+          </p>
+        </div>
+        <div className="img">
+          <img src="src/Imgs/Meeeeeeeeee.jpeg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
